bee396ad69d2e19a430bd4370d9947fd7c18c97a
ketovx <ketov-x@yandex.ru>
2020-08-30 23:03:17 +0300

    Переделан 3-й урок по Go, за основу взяты уроки Мигеля Гринберга по Flask.

--- lesson_3/golang/cmd/Application/main.go
+++ lesson_3/golang/cmd/Application/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	"github.com/PaperDevil/web-development-cec/tree/master/lesson_two/golang/internal/Server"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func main() {
+	// Создание сервера
 	server := Server.New(":8080")
-	server.Gin.Run()
+
+	// Настройки сервера
+	server.Gin.LoadHTMLGlob("templates/*")
+
+	// Роутинг
+	server.Gin.GET("/", func (c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Hello, my Lord!",
+		})
+	})
+
+	server.Gin.GET("/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Hello, " + name,
+		})
+	})
+
+	// Запуск сервера
+	if err := server.Gin.Run(":8081"); err != nil {
+		log.Fatal(err)
+	}
 }
